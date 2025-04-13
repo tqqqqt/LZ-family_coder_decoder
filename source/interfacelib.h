ee05ff601a037c1803d5f22e02cfdde7c36759f6
Sergey Bauman <tqqqqt@users.noreply.github.com>
2025-04-13 17:25:44 +0700

    added library for interface functions

---
+++ source/interfacelib.h
@@ -0,0 +1,13 @@
+#ifndef INTERFACELIB_H
+#define INTERFACELIB_H
+
+#include <iostream>
+#include <string>
+
+namespace ilib{
+	void inputMenu(int&);
+	void changeString(std::string&);
+	void changeSize(const std::string&, int&);
+}
+
+#endif
