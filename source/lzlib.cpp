66e21d1731f00182187acff7e2e10c88aa07bbb4
Sergey Bauman <tqqqqt@users.noreply.github.com>
2025-04-13 17:26:06 +0700

    start write library for lz functions

---
+++ source/lzlib.cpp
@@ -0,0 +1,17 @@
+#include "lzlib.h"
+
+void lzlib::codeLZ77(){
+	std::cout<<"LZ77";
+}
+
+void lzlib::codeLZSS(){
+	std::cout<<"LZSS";
+}
+
+void lzlib::codeLZ78(){
+	std::cout<<"LZ78";
+}
+
+void lzlib::codeLZW(){
+	std::cout<<"LZW";
+}
---
+++ source/lzlib.h
@@ -0,0 +1,13 @@
+#ifndef LZLIB_H
+#define LZLIB_H
+
+#include <iostream>
+
+namespace lzlib{
+	void codeLZ77();
+	void codeLZSS();
+	void codeLZ78();
+	void codeLZW();
+}
+
+#endif
