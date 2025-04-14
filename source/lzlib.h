2cdf4313bffe944155b24287e6f7add2647f8943
Sergey Bauman <tqqqqt@users.noreply.github.com>
2025-04-14 17:23:35 +0700

    added params to function LZ77

--- source/lzlib.h
+++ source/lzlib.h
@@ -2,9 +2,10 @@
 #define LZLIB_H
 
 #include <iostream>
+#include <string>
 
 namespace lzlib{
-	void codeLZ77();
+	void codeLZ77(const std::string&, const int&, const int&);
 	void codeLZSS();
 	void codeLZ78();
 	void codeLZW();
