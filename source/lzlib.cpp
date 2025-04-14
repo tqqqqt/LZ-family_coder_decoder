b733d8280ee096d1acea1dc91647c0739c625365
Sergey Bauman <tqqqqt@users.noreply.github.com>
2025-04-14 17:42:40 +0700

    complete LZSS and added some fix for LZ77

--- source/lzlib.cpp
+++ source/lzlib.cpp
@@ -3,9 +3,7 @@
 void lzlib::codeLZ77(const std::string& _str, const int& dict_size, const int& buffer_size){
 	size_t len=_str.length();
 
-	if(len==0) throw "incorect string";
-	if(dict_size==0) throw "incorect dictionary size";
-	if(buffer_size==0) throw "incorect buffer size";
+	if(len==0) throw "empty string";
 
 	std::string dict="";
 	int buffer_l=0, index=0, count_symb=0;
@@ -18,7 +16,7 @@ void lzlib::codeLZ77(const std::string& _str, const int& dict_size, const int& b
 
 		// try find combination
 		for(int i=1;i<=buffer_size;++i){
-			if((buffer_l+i)>=static_cast<int>(len)) break;
+			if((buffer_l+(i-1))>=static_cast<int>(len)) break;
 
 			temp=dict.rfind(_str.substr(buffer_l,i));
 			if(temp!=std::string::npos){
@@ -47,8 +45,44 @@ void lzlib::codeLZ77(const std::string& _str, const int& dict_size, const int& b
 	std::cout<<dict<<"| |\n";
 }
 
-void lzlib::codeLZSS(){
-	std::cout<<"LZSS";
+void lzlib::codeLZSS(const std::string& _str, const int& dict_size, const int& buffer_size){
+	size_t len=_str.length();
+
+	if(len==0) throw "empty string";
+
+	std::string dict;
+	int buffer_index=0, index=0, count_symb=0;
+	size_t temp=0;
+
+	while(buffer_index<static_cast<int>(len)){
+		count_symb=0;
+		index=0;
+
+		// try find combination
+		for(int i=1;i<=buffer_size;++i){
+			if((buffer_index+(i-1))>=static_cast<int>(len)) break;
+
+			temp=dict.find(_str.substr(buffer_index,i));
+			if(temp!=std::string::npos){
+				index=temp;
+				++count_symb;
+				continue;
+			}
+			break;
+		}
+
+		std::cout<<dict<<'|'<<_str.substr(buffer_index,buffer_size)<<'|';
+		if(count_symb==0) std::cout<<"0,'"<<_str[buffer_index]<<"'\n";
+		else std::cout<<"1,<"<<index<<','<<count_symb<<">\n";
+
+		if(count_symb==0) count_symb+=1;
+		dict+=_str.substr(buffer_index,count_symb);
+		buffer_index+=count_symb;
+
+		// cut dictionary if need
+		if(dict.length()>static_cast<size_t>(dict_size)) dict=dict.substr(dict.length()-dict_size,dict_size);
+	}
+	std::cout<<dict<<"| |\n";
 }
 
 void lzlib::codeLZ78(){
