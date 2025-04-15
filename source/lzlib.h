29e453e582408fdbf817a1f055a4d74b3e68d95b
Sergey Bauman <tqqqqt@users.noreply.github.com>
2025-04-15 14:33:17 +0700

    create functions to decode and add params to decodeLZ77

--- source/lzlib.h
+++ source/lzlib.h
@@ -10,6 +10,11 @@ namespace lzlib{
 	void codeLZSS(const std::string&, const int&, const int&);
 	void codeLZ78(const std::string&);
 	void codeLZW(const std::string&);
+
+	void decodeLZ77(const std::string&, const int&, const int&);
+	void decodeLZSS();
+	void decodeLZ78();
+	void decodeLZW();
 }
 
 #endif
